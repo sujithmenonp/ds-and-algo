b5e72c52581aa90d0f6447edf2a209c8fbef7bd2
Sujith Menon <sujithmenonp@gmail.com>
2020-03-15 20:45:49 +0530

    clean up

--- src/BinaryOperator.java
+++
@@ -1,7 +0,0 @@
-public class BinaryOperator {
-
-    public static void main(String[] args){
-        int a=1;
-        System.out.println(a<<1|1);
-    }
-}
--- src/README.md
+++ src/README.md
@@ -1,2 +1,1 @@
 # ds-and-algo
-1. <a href ="Pole Climbing.md">Hari Climbing a pole (logical, maths)</a>
